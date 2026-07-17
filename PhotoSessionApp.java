bd5a179c9eda6c2116c04ea31b66eb841d125695
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-07-17 09:33:31 -0400

    Add beggining comments

--- PhotoSessionApp.java
+++ PhotoSessionApp.java
@@ -1,7 +1,10 @@
 /**
  * PhotoSessionApp.java
- * Final Project: Nature Photography Session Log
+ * JavaFX GUI for Nature Photography Session Log
+ * Provide GUI to add sessions, view sessions,
+ * search by location/species, and view statistics
+ * 
  * Author: Kira Emmett Buck
  * Course: Java - Ivy Tech Community College
- * 
+ * Final Project: Nature Photography Session Log
  */
