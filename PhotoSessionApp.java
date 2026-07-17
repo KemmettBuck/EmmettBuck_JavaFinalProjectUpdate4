d8cdcaebbfc618452e48e260f776dde0e37f1bb3
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-07-17 09:43:24 -0400

    add session form fields in PhotSessionApp.java

--- PhotoSessionApp.java
+++ PhotoSessionApp.java
@@ -8,3 +8,40 @@
  * Course: Java - Ivy Tech Community College
  * Final Project: Nature Photography Session Log
  */
+import javax.swing.text.TableView;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class PhotoSessionApp extends Application {
+
+    // Data manager - holds all sessions
+    private SessionLog log = new SessionLog();
+
+    // Table displays sessions on main screen
+    private TableView<WildlifeSession> tableView = new TableView<>();
+
+    // Status Label
+    private Label lblStatus = new Label("Nature Photography Session Log");
+
+    // Text area for stats output
+    private TextArea taOutput = new TextArea();
+
+    // ------ Session Form Fields ------
+    private TextField tfDate        = new TextField();
+    private TextField tfLocation    = new TextField();
+    private TextField tfWeather     = new TextField();
+    private TextField tfEquipment   = new TextField();
+    private TextField tfDuration    = new TextField();
+    private TextField tfSpecies     = new TextField();
+    private TextField tfNumber      = new TextField();
+    private CheckBox  cbInMotion    = new CheckBox("Subject was in Motion");
+    private TextField tfShotDesc    = new TextField();
+    private TextArea  taNotes       = new TextArea();
+}
